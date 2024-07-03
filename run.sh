686a2e61a9e2b86b2f80a5dfe8321ccb6c86b475
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-07-03 10:33:57 -0500

    wip

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
-docker build -t test .
-docker run --env-file .env -v $PWD/diag:/tmp/profiler -p 8080:8080 test
+docker build -t java_otel_test .
+docker stop java_otel_test
+docker rm java_otel_test
+docker run -d --name=java_otel_test --env-file .env -v $PWD/diag:/tmp/profiler -p 8080:8080 java_otel_test
+sleep 10
+
+for i in {1..10}
+do
+    curl 127.0.0.1:8080/items
+    sleep 1
+done
+
+sleep 5
+docker stop java_otel_test
+docker logs java_otel_test >& diag/otel.log
+tar czf otel_logs.tar.gz diag
